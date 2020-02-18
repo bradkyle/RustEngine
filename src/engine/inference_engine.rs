586e32ba6d5cf9615ffa677a3739fe3d94e3f16b
Brad Duncan <bradkyleduncan@gmail.com>
2020-02-18 23:22:45 +0200

    Scaffolding out BaseEngine

--- src/engine/inference_engine.rs
+++ src/engine/inference_engine.rs
@@ -172,4 +172,6 @@ impl BaseEngine {
         }
     }
 
+
+
 }
--- src/engine/state_engine.rs
+++ src/engine/state_engine.rs
@@ -3,3 +3,38 @@
 struct StateEngine {
 
 }
+
+impl StateEngine {
+
+    fn process_order_udpate(&self, order Order) {
+
+    }
+
+    fn process_instrument_update(&self, instrument Instrument) {
+
+    }
+
+    fn process_orderbook_update(&self, depth Depth) {
+
+    }
+
+    fn process_margin_update(&self, margin Margin) {
+
+    }
+
+    fn process_position_update(&self, position Position) {
+
+    }
+
+    fn process_tradebin_update(&self, tradebin TradeBin) {
+
+    }
+
+    fn process_trade_update(&self, trade Trade) {
+
+    }
+
+    fn run(&self) {
+
+    }
+}
